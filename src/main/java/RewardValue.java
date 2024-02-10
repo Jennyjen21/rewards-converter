a4d8df0f51832bd1d4aea154b484aade6c587db7
Jennifer Romain <149035475+Jennyjen21@users.noreply.github.com>
2024-02-10 12:36:18 -0500

    Commit Changes

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    private double cashValue;
+    private int milesValue;
+
+    public RewardValue(double cashVaue) {
+        this.cashValue = cashValue;
+        this.milesValue = (int) Math.round(cashValue * 3500);
+    }
+
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue / 3500.0;
+    }
+
+    public double getCasValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return milesValue;
+    }
+
+    public double convertMilesToCash() {
+        return milesValue * 0.0035;
+    }
 }
